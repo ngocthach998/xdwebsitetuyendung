83fe61e488806fd63539db07fcdd673311bcdf23
tuchithanh1998 <tuchithanh1998@gmail.com>
2020-06-16 15:18:04 +0700

    layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="../../../../favicon.ico">
+
+		<title>Tuyển Dụng STU</title>
+
+		<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="stylesheet" href="admin.css">
+		<!-- Custom styles for this template -->
+		<link href="navbar-top-fixed.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-white">
+			<a class="navbar-brand" href=""><img class="imgwh" src="logo.png"></a>
+
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarCollapse">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item ">
+						<a class="nav-link" href="#">Đăng Nhập</a>
+					</li>
+					<li class="nav-item ">
+						<a class="nav-link" href="#">Đăng Ký</a>
+					</li>
+					
+				</ul>
+
+
+				<ul class="navbar-nav mr-2 ">
+					<li class="nav-item ">
+						<a class="nav-link" href="#">Dành Cho Nhà Tuyển Dụng</a>
+					</li>
+					
+				</ul>
+
+
+				
+			</div>
+		</nav>
+
+		<div class="container-fluid" >
+			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner" id="container1">
+    <div class="carousel-item active">
+      <img src="logo.png" class="d-block w-100" style="height: 350px;" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="logo.png" class="d-block w-100" style="height: 350px;" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="logo.png" class="d-block w-100" style="height: 350px;" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+<div id="container" class="container">
+            <div class="row">
+                <div class="col-sm-10 offset-sm-1 text-center">
+                    <h1 class="display-3">Tìm Việc Nhanh</h1>
+                    <div class="info-form">
+                        <form action="" class="form-inline justify-content-center">
+                            <div class="form-group">
+                                <label class="sr-only">Name</label>
+                                <input type="text" class="form-control" placeholder="Tên công việc">
+                            </div>
+                            <div class="form-group">
+                                <label class="sr-only">Ngành nghề</label>
+                                <select class="form-control form-control">
+  	<option selected>Ngành nghề</option>
+  	<option value="1">CNTT</option>
+  	<option value="2">QTKD</option>
+  	<option value="3">KTTC</option>
+  </select>
+                            </div>
+                            <div class="form-group">
+                                <label class="sr-only">Mức lương</label>
+                                 <select class="form-control form-control">
+  	<option selected>Mức lương</option>
+  	<option value="1">One</option>
+  	<option value="2">Two</option>
+  	<option value="3">Three</option>
+  </select>
+                            </div>
+                            <button type="submit" class="btn btn-success ">Tìm Kiếm</button>
+                        </form>
+                    </div>
+                    <br>
+
+                  
+                </div>
+            </div>
+        </div>
+
+        <div class=" bg-light">
+        <div id="container" class="container container-vieclam bg-light">
+        	  <div class="col-sm-10 offset-sm-1 text-center">
+        	 <h3 class="display-3">Việc Làm Nổi Bật</h3>      </div>
+            <div class="row ">
+
+            	<div class="col-sm-6"  style="padding-top: 30px;">
+<div class="card">
+        <div class="row no-gutters">
+            <div class="col-auto">
+                <img src="//placehold.it/120" class="img-fluid" alt="">
+            </div>
+            <div class="col">
+                <div class="card-block px-2">
+                   <h5 class="card-title ">Kế toán xuất nhập khẩu</h5>
+                   <p class="card-text">	<a href="" style="color:#6C757D;"> Công ty đầu tư NSEF</a></p>
+                   
+
+					<ul class="list-inline">
+			<li class="list-inline-item"><p class="card-text "> <img src="upload\img\layout\dollar-sign.svg"> 5-10 triệu </p></li>
+				<li class="list-inline-item"><p class="card-text "> <img src="upload\img\layout\clock.svg">Hạn chót: 30/8/2020</p></li>
+			</ul> 
+                </div>
+            </div>
+        </div>
+        
+  </div>
+    <div class="card" style="margin-top: 30px;">
+        <div class="row no-gutters">
+            <div class="col-auto">
+                <img src="//placehold.it/120" class="img-fluid" alt="">
+            </div>
+            <div class="col">
+                <div class="card-block px-2">
+                   <h5 class="card-title ">Kế toán xuất nhập khẩu</h5>
+                   <p class="card-text">	<a href="" style="color:#6C757D;"> Công ty đầu tư NSEF</a></p>
+                   
+
+					<ul class="list-inline">
+			<li class="list-inline-item"><p class="card-text "> <img src="upload\img\layout\dollar-sign.svg"> 5-10 triệu </p></li>
+				<li class="list-inline-item"><p class="card-text "> <img src="upload\img\layout\clock.svg">Hạn chót: 30/8/2020</p></li>
+			</ul> 
+                </div>
+            </div>
+        </div>
+        
+  </div>
+            	</div>
+
+            	<div class="col-sm-6" style="padding-top: 30px;">
+		  	  	 <div class="card">
+        <div class="row no-gutters">
+            <div class="col-auto">
+                <img src="//placehold.it/120" class="img-fluid" alt="">
+            </div>
+            <div class="col">
+                <div class="card-block px-2">
+                   <h5 class="card-title ">Kế toán xuất nhập khẩu</h5>
+                   <p class="card-text">	<a href="" style="color:#6C757D;"> Công ty đầu tư NSEF</a></p>
+                   
+
+					<ul class="list-inline">
+			<li class="list-inline-item"><p class="card-text "> <img src="upload\img\layout\dollar-sign.svg"> 5-10 triệu </p></li>
+				<li class="list-inline-item"><p class="card-text "> <img src="upload\img\layout\clock.svg">Hạn chót: 30/8/2020</p></li>
+			</ul> 
+                </div>
+            </div>
+        </div>
+        
+  </div>
+            	
+
+		  	  	 <div class="card" style="margin-top: 30px;">
+        <div class="row no-gutters">
+            <div class="col-auto">
+                <img src="//placehold.it/120" class="img-fluid" alt="">
+            </div>
+            <div class="col">
+                <div class="card-block px-2">
+                   <h5 class="card-title ">Kế toán xuất nhập khẩu</h5>
+                   <p class="card-text">	<a href="" style="color:#6C757D;"> Công ty đầu tư NSEF</a></p>
+                   
+
+					<ul class="list-inline">
+			<li class="list-inline-item"><p class="card-text "> <img src="upload\img\layout\dollar-sign.svg"> 5-10 triệu </p></li>
+				<li class="list-inline-item"><p class="card-text "> <img src="upload\img\layout\clock.svg">Hạn chót: 30/8/2020</p></li>
+			</ul> 
+                </div>
+            </div>
+        </div>
+        
+  </div>
+            	</div>
+            </div>
+        </div>
+  </div>
+
+
+<div id="container" class="container container-nganhnghe">
+        	 <h3 class="display-6 a5sd">Việc làm theo ngành nghề</h3>      
+            <div class="row" >
+            	<div class="col-sm-4">
+			<ul class="list-unstyled">
+				<li><a href="">Lập trình viên (5)</a></li>
+				<li><a href="">Lập trình viên (5)</a></li>
+				<li><a href="">Lập trình viên (5)</a></li>
+				<li><a href="">Lập trình viên (5)</a></li>
+			</ul>       	  	  	  	 
+            	</div>
+            	<div class="col-sm-4">
+			<ul class="list-unstyled">
+				<li><a href="">Lập trình viên (5)</a></li>
+				<li><a href="">Lập trình viên (5)</a></li>
+				<li><a href="">Lập trình viên (5)</a></li>
+				<li><a href="">Lập trình viên (5)</a></li>
+			</ul>  	  	  	 
+            	</div>
+            	<div class="col-sm-4">
+			<ul class="list-unstyled">
+				<li><a href="">Lập trình viên (5)</a></li>
+				<li><a href="">Lập trình viên (5)</a></li>
+				<li><a href="">Lập trình viên (5)</a></li>
+				<li><a href="">Lập trình viên (5)</a></li>
+			</ul>  	  	  	  	 
+            	</div>
+            </div>
+        </div>
+
+<div class="container-fluid" id="container2" style="background-color: #66CCFF;">
+	<div class="row ">
+		<div class="col-sm-3">
+		  	<img src="logo.png" class="d-block w-100" alt="...">
+		</div>
+		<div class="col-sm-3 ">
+		  	<ul class="list-unstyled">
+				<li>Từ Chí Thành</li>
+				<li>Phạm Ngọc Thạch</li>
+			</ul>  	  	  	  	 
+		</div>
+		<div class="col-sm-3">
+		  	
+		</div>
+		<div class="col-sm-3">
+		  	
+		</div>
+	</div>
+</div>
+
+		<!-- Bootstrap core JavaScript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<script src="../../../../assets/js/ie10-viewport-bug-workaround.js"></script>
+	</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin/layout');
+    return view('index');
+});
+Route::get('/index', function () {
+    return view('index');
 });
